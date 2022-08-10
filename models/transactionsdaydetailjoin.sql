0aa6db4d036e517e6acccee9514436aa1baa8e8e
msft.ora@gmail.com <msft.ora@gmail.com>
2022-08-10 18:52:55 +0000

    test

---
+++ models/transactionsdaydetailjoin.sql
@@ -0,0 +1,23 @@
+{{ config(materialized="table") }}
+
+
+
+WITH
+
+transactionsdaydetail_schema AS
+
+(SELECT SITEID
+
+FCID
+
+    FROM HVMG.transactionsdaydetail
+
+    union
+
+    select SITEID
+
+FCID from LANDING.transactionsdaydetail)
+
+
+
+select * from transactionsdaydetail_schema
