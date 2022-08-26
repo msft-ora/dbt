0e392df78ce4d5b0a588e89cb659a2a5ed72255e
sam.wang@improving.com <sam.wang@improving.com>
2022-08-26 20:03:27 +0000

    added union for transactions and transactionsmonth

--- models/Analytics/transactionsday.sql
+++ models/Analytics/transactionsday.sql
@@ -20,3 +20,13 @@ SELECT
 FROM poc.hvmg_raw."FC.TRANSACTIONSDAYDETAIL" t
 join poc.analytics.clients c
   on t.client_nm =c.name
+
+  union all
+ select c.clientid,s.siteID,-3,t.date,getdate(),null,i.itemid,t.qty,t.Amt,0,0,current_timestamp ROW_INSERT_TS from 
+poc.hvmg_raw.TRANSACTIONS t
+join poc.analytics.clients c
+  on t.client_nm =c.name
+join poc.hvmg_raw.sites s
+on t.siteid = s.siteTag     and t.client_nm=s.client_nm
+join poc.hvmg_raw.items i
+on  t.Tag = i.itemTag     and t.client_nm=i.client_nm
--- models/Analytics/transactionsperiod.sql
+++ models/Analytics/transactionsperiod.sql
@@ -9,7 +9,7 @@ SELECT
   --, t.CALENDARID CALENDARTYPEID
   , t.SITEID
   , t.FCID
-  , t."YEAR"
+  , t.YEAR
   , t.PERIOD
   , t.TIMECHANGE
   , t.TIMECHANGEEND
@@ -23,3 +23,12 @@ SELECT
 FROM POC.hvmg_raw."FC.TRANSACTIONSPERIOD" t
 join poc.analytics.clients c
   on t.client_nm =c.name
+union all
+select c.clientid,s.siteID,-3,year,month,getdate(),null,i.itemid,qty,Amt,0,0,1,current_timestamp ROW_INSERT_TS from 
+poc.hvmg_raw.TRANSACTIONSMONTH t
+join poc.analytics.clients c
+  on t.client_nm =c.name
+join poc.hvmg_raw.sites s
+on t.siteid = s.siteTag     and t.client_nm=s.client_nm
+join poc.hvmg_raw.items i
+on  t.Tag = i.itemTag     and t.client_nm=i.client_nm
