a83f4cc4263377cd919a1dceb53d0f99a6c69118
LAPTOP-8NLHPREH\Sam.Wang <sam.wang@improving.com>
2022-09-08 14:44:51 -0400

    test

--- models/transactionsperiod_raw.sql
+++
@@ -1,9 +0,0 @@
-{{
-  config(materialized='incremental'  ,
-  pre_hook="delete from {{this}} t using {{ ref('transactionsperiod_temp') }} s 
-  where t.client_nm=s.client_nm and t.siteid=s.siteid and t.fcid=s.fcid and t.year=s.year and t.period=s.period and t.itemid=s.itemid and t.TimeChangeEnd=s.TimeChange and t.TimeChangeEnd is null  "
-
-)
-}}
-
-select {{ gen_transactionsperiod_raw( 'HVMG', 'HVMG_TRANSACTIONSPERIOD_2022-08-18_210301.json' ) }}
--- models/transactionsperiod_temp.sql
+++
@@ -1,6 +0,0 @@
-{{
-  config(materialized='view')
-}}
-
-
-select {{ gen_transactionsperiod_landing( 'HVMG', 'HVMG_TRANSACTIONSPERIOD_2022-08-18_210301.json' ) }}
